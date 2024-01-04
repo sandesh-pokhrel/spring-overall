89f4d04f37669f6d18f9220b3b72086717c9da39
Sandesh Pokhrel <sandesh.pokhrel56@gmail.com>
2024-01-04 17:45:55 +0545

    logging through opentelemetry - loki - grafana completed

---
+++ src/main/java/com/sandesh/overall/config/OpenTelemetryConfig.java
@@ -0,0 +1,55 @@
+package com.sandesh.overall.config;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.LogRecordProcessor;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.SdkLoggerProviderBuilder;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.semconv.ResourceAttributes;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenTelemetryConfig {
+
+    @Bean
+    OpenTelemetry openTelemetry(final SdkLoggerProvider sdkLoggerProvider, final SdkTracerProvider sdkTracerProvider, final ContextPropagators contextPropagators) {
+        final OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
+                .setLoggerProvider(sdkLoggerProvider)
+                .setTracerProvider(sdkTracerProvider)
+                .setPropagators(contextPropagators)
+                .build();
+        OpenTelemetryAppender.install(openTelemetrySdk);
+        return openTelemetrySdk;
+    }
+
+    @Bean
+    SdkLoggerProvider otelSdkLoggerProvider(final ObjectProvider<LogRecordProcessor> logRecordProcessors,
+                                            @Value("spring.application.name") String applicationName) {
+        final Resource resource = Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, applicationName));
+        final SdkLoggerProviderBuilder builder = SdkLoggerProvider.builder()
+                .setResource(Resource.getDefault().merge(resource));
+        logRecordProcessors.orderedStream().forEach(builder::addLogRecordProcessor);
+        return builder.build();
+    }
+
+    @Bean
+    LogRecordProcessor otelLogRecordProcessor() {
+        return BatchLogRecordProcessor
+                .builder(
+                        OtlpGrpcLogRecordExporter.builder()
+                                .setEndpoint("http://localhost:4317")
+                                .build())
+                .build();
+    }
+}
