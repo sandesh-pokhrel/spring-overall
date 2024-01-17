cdc11c3a3ee2dbb495ee24a7bbbdcac74435cfc8
Sandesh Pokhrel <sandesh.pokhrel56@gmail.com>
2024-01-17 18:10:06 +0545

    spring integration - employee -> address wire tapping added

--- src/main/java/com/sandesh/overall/config/integration/EmployeeIntegrationConfig.java
+++ src/main/java/com/sandesh/overall/config/integration/EmployeeIntegrationConfig.java
@@ -6,18 +6,21 @@ import com.sandesh.overall.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.core.GenericHandler;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.MessageChannels;
-import org.springframework.integration.kafka.dsl.Kafka;
-import org.springframework.integration.splitter.AbstractMessageSplitter;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.support.MessageBuilder;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class EmployeeIntegrationConfig {
@@ -26,6 +29,7 @@ public class EmployeeIntegrationConfig {
     private final KafkaTemplate<Long, Employee> employeeKafkaTemplate;
     private static final String EMPLOYEE_CHANNEL = "employeeChannel";
     private static final String ADDRESS_CHANNEL = "addressChannel";
+    private static final String LOGGING_CHANNEL = "loggingChannel";
 
     public EmployeeIntegrationConfig(EmployeeService employeeService,
                                      @Qualifier("employeeKafkaTemplate") KafkaTemplate<Long, Employee> employeeKafkaTemplate) {
@@ -33,11 +37,11 @@ public class EmployeeIntegrationConfig {
         this.employeeKafkaTemplate = employeeKafkaTemplate;
     }
 
-    @Bean
+    // @Bean
     public ApplicationRunner employeeSender(MessageChannel employeeChannel) {
         return args -> {
             List<Employee> employees = employeeService.getAll();
-            employeeChannel.send(MessageBuilder.withPayload(employees).build());
+            employeeChannel.send(MessageBuilder.withPayload(employees).setHeader("size", employees.size()).build());
         };
     }
 
@@ -51,6 +55,11 @@ public class EmployeeIntegrationConfig {
         return MessageChannels.direct().getObject();
     }
 
+    @Bean(name = LOGGING_CHANNEL)
+    public MessageChannel loggingChannel() {
+        return MessageChannels.direct().getObject();
+    }
+
     @Bean
     public IntegrationFlow employeeFlow() {
         return IntegrationFlow
@@ -60,7 +69,12 @@ public class EmployeeIntegrationConfig {
                     payload.forEach(e -> System.out.println(e.getName()));
                     return payload;
                 })
-                // .handle(Kafka.outboundChannelAdapter(employeeKafkaTemplate)).get();
+                // .handle(Kafka.outboundChannelAdapter(employeeKafkaTemplate)).get(); // sending to kafka topic , kafka template is configured with default topic in this case
+                .split((Function<List<Employee>, List<List<Address>>>) employees ->
+                        employees.stream().map(Employee::getAddresses)
+                                .filter(Objects::nonNull)
+                                .collect(Collectors.toList()))
+                /* Using message splitter
                 .split(new AbstractMessageSplitter() {
                     @Override
                     protected Object splitMessage(Message<?> message) {
@@ -69,7 +83,7 @@ public class EmployeeIntegrationConfig {
                                 .filter(Objects::nonNull)
                                 .collect(Collectors.toList());
                     }
-                })
+                })*/
                 .channel(ADDRESS_CHANNEL).get();
     }
 
@@ -77,11 +91,20 @@ public class EmployeeIntegrationConfig {
     public IntegrationFlow addressFlow() {
         return IntegrationFlow
                 .from(ADDRESS_CHANNEL)
+                .wireTap(LOGGING_CHANNEL)
                 .handle((GenericHandler<List<Address>>) (payload, headers) -> {
                     System.out.println("Address message ::: ");
                     payload.forEach(a -> System.out.println(a.getCity()));
                     return null;
                 })
                 .get();
+    }
+
+    @ServiceActivator(inputChannel = LOGGING_CHANNEL)
+    public void log(@Payload Object payload, @Headers MessageHeaders headers) {
+        System.out.println("----Wiretapping----");
+        System.out.println("Payload: " + payload);
+        System.out.println("Headers:");
+        headers.forEach((k, v) -> System.out.println(k + ":" + v));
     }
 }
